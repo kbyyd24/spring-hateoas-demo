109b71429e1ee4eb8c66835bb2aaf8ceb5bb2a65
kbyyd24 <melo@gaoyuexiang.cn>
2019-10-23 01:15:26 +0800

    [crud] allow cors

---
+++ src/main/java/cn/gaoyuexiang/hateoas/weibo/demo/hateoasdemo/config/MvcConfig.java
@@ -0,0 +1,13 @@
+package cn.gaoyuexiang.hateoas.weibo.demo.hateoasdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowCredentials(true).allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+  }
+}
